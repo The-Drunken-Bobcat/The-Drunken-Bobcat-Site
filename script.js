cbb9e68e8689b763589d5cd5c021eca7bac77d42
RockyTheProtogen <124413320+RockyTheProtogen@users.noreply.github.com>
2024-07-28 12:35:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,6 @@ document.addEventListener('mousemove', (event) => {
 function updateGradient() {
     currentX += (mouseX - currentX) * 0.0005;
     currentY += (mouseY - currentY) * 0.0005;
-    document.body.style.background = `radial-gradient(circle at ${currentX}px ${currentY}px, rgb(0, 52, 148), rgb(0, 0, 0))`;
+    document.body.style.background = `radial-gradient(circle at ${currentX}px ${currentY}px, rgb(255, 5, 180), rgb(0, 0, 0))`;
     requestAnimationFrame(updateGradient);
 }
